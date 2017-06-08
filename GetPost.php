636cb822cef7689506dba67e48a28585a970efed
MinhUchiha <nqminh27894@gmail.com>
2017-06-09 00:41:28 +0700

    fix point 2

--- GetPost.php
+++ GetPost.php
@@ -6,7 +6,6 @@ $classid= $_POST["classid"];
 $statement = mysqli_prepare($conn, "SELECT * FROM post WHERE classid = ? ORDER BY timestamp DESC") or die(mysqli_error($conn));
 mysqli_stmt_bind_param($statement, "s", $classid);
 $statement->execute();
-echo "1";
 $array = array();
 $response = array();
 $response["success"] = true;
